b5afd3be7dbeb576e4b65e4faf0d78a08c9bf0e3
Chandrakant <chandrakant@Chandrakants-MacBook-Air.local>
2020-03-07 12:35:13 +0530

    custom toolbar

---
+++ app/src/main/java/com/ppstudios/customtoolbar/MainActivity.java
@@ -0,0 +1,41 @@
+package com.ppstudios.customtoolbar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_group:
+                Toast.makeText(MainActivity.this, "New Group", Toast.LENGTH_SHORT).show();
+            case R.id.whatsapp:
+                Toast.makeText(MainActivity.this, "Whatsapp", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
